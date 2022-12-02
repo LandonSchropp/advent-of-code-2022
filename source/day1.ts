13b99cac08510975b38006bddf75fb41e6cb4cb0
Landon Schropp <schroppl@gmail.com>
2022-12-01 18:25:01 -0600

    Implement day 1 part 2

--- source/day1.ts
+++ source/day1.ts
@@ -1,10 +1,30 @@
-function parseAndSumGroup(group: string) {
-  return group
-    .split("\n")
-    .map(word => parseInt(word, 10))
-    .reduce((accumulator, number) => accumulator + number);
+function add(first: number, second: number) {
+  return first + second;
+}
+
+function sum(numbers: number[]) {
+  return numbers.reduce(add);
+}
+
+function parse(input: string): number[][] {
+  return input
+    .split("\n\n")
+    .map(group => {
+      return group
+        .split("\n")
+        .map(word => parseInt(word, 10));
+    });
 }
 
 export function part1(input: string) {
-  return Math.max(...input.split("\n\n").map(parseAndSumGroup));
+  return Math.max(...parse(input).map(sum));
+}
+
+export function part2(input: string) {
+  return parse(input)
+    .map(sum)
+    .sort((a, b) => a - b)
+    .reverse()
+    .slice(0, 3)
+    .reduce(add);
 }
--- test/day1.test.ts
+++ test/day1.test.ts
@@ -1,32 +1,39 @@
-import { part1 } from "../source/day1";
+import { part1, part2 } from "../source/day1";
 import { formatInput } from "./helpers";
 
-let input: string;
+const EXAMPLE_INPUT = formatInput(`
+  1000
+  2000
+  3000
+
+  4000
+
+  5000
+  6000
+
+  7000
+  8000
+  9000
+
+  10000
+`);
 
 describe("part1", () => {
 
   describe("when the input is the example provided in the problem", () => {
-    beforeEach(() => {
-      input = formatInput(`
-        1000
-        2000
-        3000
-
-        4000
-
-        5000
-        6000
-
-        7000
-        8000
-        9000
-
-        10000
-      `);
-    });
 
     it("produces the expected example solution", () => {
-      expect(part1(input)).toEqual(24000);
+      expect(part1(EXAMPLE_INPUT)).toEqual(24000);
+    });
+  });
+});
+
+describe("part2", () => {
+
+  describe("when the input is the example provided in the problem", () => {
+
+    it("produces the expected example solution", () => {
+      expect(part2(EXAMPLE_INPUT)).toEqual(45000);
     });
   });
 });
