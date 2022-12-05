592ae242ea43d709c292371f568082a8a869873e
Landon Schropp <schroppl@gmail.com>
2022-12-05 00:29:18 -0600

    Implement day 3 part 2

--- source/day3.ts
+++ source/day3.ts
@@ -4,17 +4,20 @@
 // ^^^^^^^  ^^^^^^^  ^^^^^^^  ^^^^^^^  ^^^^^^^  ^^^^^^^  ^^^^^^^  ^^^^^^^
 //   |||      |||      |||      |||      |||      |||      |||      |||
 
-import { intersection, sum } from "lodash-es";
+import { chunk, intersection, sum } from "lodash-es";
 
 const UPPER_A_CODE = "A".charCodeAt(0);
 const LOWER_A_CODE = "a".charCodeAt(0);
 const NUMBER_OF_LETTERS = 26;
 
-function parse(input: string): [string[], string[]][] {
+function parse(input: string): string[][] {
   return input
     .split("\n")
-    .map(line => line.split(""))
-    .map(line => [ line.slice(0, line.length / 2), line.slice(line.length / 2) ]);
+    .map(line => line.split(""));
+}
+
+function divideInHalf(items: string[]): [ string[], string[] ] {
+  return [ items.slice(0, items.length / 2), items.slice(items.length / 2) ];
 }
 
 function priority(character: string) {
@@ -23,10 +26,14 @@ function priority(character: string) {
     : character.charCodeAt(0) - UPPER_A_CODE + NUMBER_OF_LETTERS + 1;
 }
 
-function intersectionPrioritySum([ compartment1, compartment2 ]: [string[], string[]]) {
-  return sum(intersection(compartment1, compartment2).map(priority));
+function intersectionPrioritySum(items: string[][]) {
+  return sum(intersection(...items).map(priority));
 }
 
 export function part1(input: string) {
-  return sum(parse(input).map(intersectionPrioritySum));
+  return sum(parse(input).map(divideInHalf).map(intersectionPrioritySum));
+}
+
+export function part2(input: string) {
+  return sum(chunk(parse(input), 3).map(intersectionPrioritySum));
 }
--- test/day3.test.ts
+++ test/day3.test.ts
@@ -1,4 +1,4 @@
-import { part1 } from "../source/day3";
+import { part1, part2 } from "../source/day3";
 import { formatInput } from "./helpers";
 
 const EXAMPLE_INPUT = formatInput(`
@@ -16,6 +16,16 @@ describe("part1", () => {
 
     it("produces the expected example solution", () => {
       expect(part1(EXAMPLE_INPUT)).toEqual(157);
+    });
+  });
+});
+
+describe("part2", () => {
+
+  describe("when the input is the example provided in the problem", () => {
+
+    it("produces the expected example solution", () => {
+      expect(part2(EXAMPLE_INPUT)).toEqual(70);
     });
   });
 });
