62f66a798a84165210eefb7fefa0066003b5c0d5
Landon Schropp <schroppl@gmail.com>
2022-12-05 11:04:55 -0600

    Improve the formatInput helper
    * The helper now correctly maintains whitespace
    * The helper's unindent algorithm is smarter—it properly handles lines
      with whitespace.

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,9 +1,42 @@
+import { dropRightWhile, dropWhile, min, negate } from "lodash-es";
+
+/**
+ * Returns true if the string is blank.
+ */
+function isBlank(input: string): boolean {
+  return /^\s*$/.test(input);
+}
+
+/**
+ * Removes empty lines from the beginning and end of a string without affecting other lines or their
+ * whitespace.
+ */
+function trimEmptyLines(input: string): string {
+  return dropRightWhile(dropWhile(input.split("\n"), isBlank), isBlank).join("\n");
+}
+
+/**
+ * Unindents a string.
+ */
+function unindent(input: string): string {
+  const minIndent = min(
+    input
+      .split("\n")
+      .filter(negate(isBlank))
+      .map(line => line.replace(/[^\n ].*$/, ""))
+      .map(whitespace => whitespace.length)
+  );
+
+  return input
+    .split("\n")
+    .map(line => line.replace(new RegExp(`^ {${ minIndent }}`), ""))
+    .join("\n");
+}
+
 /**
  * It's often convenient to record constant test input with extra whitespace for clarity. This
  * function formats the input so it matches the input files provided by Advent of Code.
  */
 export function formatInput(input: string): string {
-  return input
-    .trim()
-    .replaceAll(/^[ ]+/gm, "");
+  return unindent(trimEmptyLines(input));
 }
