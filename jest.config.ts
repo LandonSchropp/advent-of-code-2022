e2928a673ee6c43d6db88d04a62aa7f7b6bb1c5d
Landon Schropp <schroppl@gmail.com>
2022-12-05 00:10:01 -0600

    Allow Jest to compile lodash-es

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,10 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest/presets/js-with-ts",
   testEnvironment: "node",
-  setupFilesAfterEnv: [ "<rootDir>/test/jest.setup.js" ]
+  setupFilesAfterEnv: [ "<rootDir>/test/jest.setup.js" ],
+  transformIgnorePatterns: [
+    "<rootDir>/node_modules/(?!lodash-es)"
+  ]
 };
 
 export default config;
