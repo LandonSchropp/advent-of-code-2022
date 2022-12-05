94487e4913805db2d7bbcf97bb8fc75ef523ac25
Landon Schropp <schroppl@gmail.com>
2022-12-05 10:25:35 -0600

    Set up skeleton for day 5

---
+++ test/day5.test.ts
@@ -0,0 +1,25 @@
+import { part1, part2 } from "../source/day5";
+import { formatInput } from "./helpers";
+
+const EXAMPLE_INPUT = formatInput(`
+`);
+
+describe("part1", () => {
+
+  describe("when the input is the example provided in the problem", () => {
+
+    it("produces the expected example solution", () => {
+      expect(part1(EXAMPLE_INPUT)).toEqual(null);
+    });
+  });
+});
+
+describe("part2", () => {
+
+  describe("when the input is the example provided in the problem", () => {
+
+    it("produces the expected example solution", () => {
+      expect(part2(EXAMPLE_INPUT)).toEqual(null);
+    });
+  });
+});
