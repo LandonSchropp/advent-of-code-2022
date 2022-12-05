7557a88551ca7f21c3657d558709bb2be479ea1a
Landon Schropp <schroppl@gmail.com>
2022-12-05 10:15:22 -0600

    Add templates for solutions

---
+++ templates/source.ts
@@ -0,0 +1,18 @@
+//    ^        ^        ^        ^        ^        ^        ^        ^
+//   ^^^      ^^^      ^^^      ^^^      ^^^      ^^^      ^^^      ^^^
+//  ^^^^^    ^^^^^    ^^^^^    ^^^^^    ^^^^^    ^^^^^    ^^^^^    ^^^^^
+// ^^^^^^^  ^^^^^^^  ^^^^^^^  ^^^^^^^  ^^^^^^^  ^^^^^^^  ^^^^^^^  ^^^^^^^
+//   |||      |||      |||      |||      |||      |||      |||      |||
+
+function parse(input: string) {
+  return input
+    .split("\n");
+}
+
+export function part1(input: string) {
+  return null;
+}
+
+export function part2(input: string) {
+  return null;
+}
